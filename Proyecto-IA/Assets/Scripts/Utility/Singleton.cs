a0288c6a0193ce243075f4c45f87548142e8f344
asd <ad>
2019-09-02 22:18:48 -0300

    Singleton class

---
+++ Proyecto-IA/Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static Singleton<T> instance = null;
+
+    public static T Instance {
+        get{
+            if (instance = null)
+                instance = FindObjectOfType<Singleton<T>>();
+            return (T)instance;
+        }
+    }
+
+    protected virtual void Initialize(){
+
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(this.gameObject);
+
+        instance = this;
+
+        Initialize();
+    }
+}
