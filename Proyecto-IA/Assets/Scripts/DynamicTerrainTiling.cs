0d1d4e60a7cdba9348ef38a3d50a453c133e7419
Thekhumi <gaspacho09@hotmail.com>
2019-10-25 20:39:49 -0300

    Dynamic Tiling

---
+++ Proyecto-IA/Assets/Scripts/DynamicTerrainTiling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DynamicTerrainTiling : MonoBehaviour
+{
+    [SerializeField] float tileScaleToObjectScale = 0.5f;
+    public void updateTiling(){
+        foreach (Transform child in transform) {
+            GameObject obj = child.gameObject;
+            if (obj.layer == 9) {
+                float xScale = obj.transform.localScale.x / tileScaleToObjectScale;
+                float zScale = obj.transform.localScale.z / tileScaleToObjectScale;
+                Vector2 vec = new Vector2(xScale, zScale);
+                obj.GetComponent<Renderer>().sharedMaterial.mainTextureScale = vec;
+            }
+        }
+
+    }
+}
---
+++ Proyecto-IA/Assets/Scripts/Editor/TextureTileEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DynamicTerrainTiling))]
+public class TextureTileEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        DynamicTerrainTiling myScript = (DynamicTerrainTiling)target;
+        if (GUILayout.Button("Update Tiling")) {
+            myScript.updateTiling();
+        }
+    }
+}
