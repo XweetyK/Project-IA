47d5f184a0eb10cfbb110f8a84c07e104542077b
Thekhumi <gaspacho09@hotmail.com>
2019-10-25 21:45:40 -0300

    Terrain Types WIP

---
+++ Proyecto-IA/Assets/Scripts/Terrain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    public enum TerrainType { Land, Gravel }
+
+    [SerializeField] TerrainType type = TerrainType.Land;
+
+    public TerrainType Type{
+        get{return type; }
+        set{ type = value; }
+    }
+}
